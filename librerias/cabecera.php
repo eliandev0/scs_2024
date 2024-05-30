3e3706ab1da6c5d999285e87c15cdad3d58ee191
E L I A N <eliandev0@outlook.com>
2024-05-30 11:19:23 +0100

    Add files via upload

---
+++ librerias/cabecera.php
@@ -0,0 +1,13 @@
+<?php
+global $CONFIG_GLOBAL;
+?>
+<!-- Librería Bootstrap -->
+<link href="<?php echo $CONFIG_GLOBAL['rutaURLBaseLibrerias']?>/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Librería jQuery -->
+<script src="<?php echo $CONFIG_GLOBAL['rutaURLBaseLibrerias']?>/jQuery/jquery.min.js"></script>
+
+<!-- Librería fontawesome -->
+<link rel="stylesheet" href="<?php echo $CONFIG_GLOBAL['rutaURLBaseLibrerias']?>/fontawesome/css/all.min.css">
+
+
---
+++ librerias/footer.php
@@ -0,0 +1,4 @@
+<?php
+global $CONFIG_GLOBAL;
+?>
+<script src="<?php echo $CONFIG_GLOBAL['rutaURLBaseLibrerias'] ?>/bootstrap/js/bootstrap.bundle.min.js"></script>
